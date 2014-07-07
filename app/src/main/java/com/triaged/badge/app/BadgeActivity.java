40bc873ff26899412a9d059efd5080d89e72ce1b
Will Lerner <Will@william-lerners-macbook-pro.local>
2014-07-07 11:51:01 -0400

    boilerplate stuff

---
+++ app/src/main/java/com/triaged/badge/app/BadgeActivity.java
@@ -0,0 +1,33 @@
+package com.triaged.badge.app;
+
+import android.app.Activity;
+import android.support.v7.app.ActionBarActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+
+/**
+ * Created by Will on 7/7/14.
+ */
+public abstract class BadgeActivity extends ActionBarActivity {
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.contacts, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+}
