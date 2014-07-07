436cc64c60530f6961ab0d1e7cbe1a8f9b7e90d4
Will Lerner <Will@william-lerners-macbook-pro.local>
2014-07-07 13:02:18 -0400

    style for login page, setting up events for login page

--- app/src/main/java/com/triaged/badge/app/LoginActivity.java
+++ app/src/main/java/com/triaged/badge/app/LoginActivity.java
@@ -1,16 +1,36 @@
 package com.triaged.badge.app;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.triaged.badge.app.views.EditTextWithFont;
 
 /**
  * Created by Will on 7/7/14.
  */
 public class LoginActivity extends BadgeActivity {
 
+    private EditText loginEmail = null;
+    private EditText loginPassword = null;
+    private Button loginButton = null;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        loginEmail = (EditText) findViewById(R.id.login_email);
+        loginPassword = (EditText) findViewById(R.id.login_email);
+        loginButton = (Button) findViewById(R.id.login_button);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                
+            }
+        });
     }
 
 }
