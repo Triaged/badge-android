91d80472a8f5ededdd2d6b73acd51a1524347f79
Will Lerner <Will@william-lerners-macbook-pro.local>
2014-08-04 14:21:39 -0400

    using prod actoss the board

--- app/src/main/java/com/triaged/badge/app/BadgeApiClient.java
+++ app/src/main/java/com/triaged/badge/app/BadgeApiClient.java
@@ -36,9 +36,9 @@ public class BadgeApiClient extends DefaultHttpClient {
     private static final String PROD_API_MESSAGING_HOST = "messaging.badge.co";
     private static final String STAGING_API_MESSAGING_HOST = "badge-messaging-staging.herokuapp.com";
     //private static final String STAGING_API_MESSAGING_HOST = "10.9.8.93";
-    private static final String API_MESSAGING_HOST = STAGING_API_MESSAGING_HOST;
+    private static final String API_MESSAGING_HOST = PROD_API_MESSAGING_HOST;
 
-    private static final String API_HOST = STAGING_API_HOST;
+    private static final String API_HOST = PROD_API_HOST;
     private static final String AUTHORIZATION_HEADER_NAME = "Authorization";
 
     private static final String PATCH_ACCOUNT_URI = String.format("%s://%s/v1/account", API_PROTOCOL, API_HOST);
--- app/src/main/java/com/triaged/badge/app/FayeService.java
+++ app/src/main/java/com/triaged/badge/app/FayeService.java
@@ -34,8 +34,8 @@ public class FayeService extends Service implements FayeClient.FayeListener {
     protected static final String LOG_TAG = FayeService.class.getName();
 
     protected static final String STAGING_FAYE_HOST = "ws://messaging.badge.co/streaming";
-    protected static final String PROD_FAYE_HOST = "ws://badge-messaging.herokuapp.com/streaming";
-    protected static final String FAYE_HOST = STAGING_FAYE_HOST;
+    protected static final String PROD_FAYE_HOST = "ws://messaging.badge.co/streaming";
+    protected static final String FAYE_HOST = PROD_FAYE_HOST;
 
     //protected Fa faye;
     protected SharedPreferences prefs;
