b13e27dec12d33e42e5d4c0a25d008619846cde1
Sadegh Kazemy <sadegh.kazemy@gmail.com>
2014-09-07 15:38:51 +0430

    Added two utility class.

---
+++ app/src/main/java/com/triaged/utils/GeneralUtils.java
@@ -0,0 +1,103 @@
+package com.triaged.utils;
+
+import android.app.Activity;
+import android.app.ActivityManager;
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.view.inputmethod.InputMethodManager;
+
+import com.triaged.badge.app.App;
+
+import java.util.List;
+
+/**
+ * Provides some general utility methods for android.
+ * <p/>
+ * Created by Sadegh Kazemy on 9/7/14.
+ */
+public class GeneralUtils {
+
+    private GeneralUtils() {
+    }
+
+    /**
+     * Determine whether the device is connected to a network or not.
+     *
+     * @param context the context its caller.
+     * @return true if the device is connected or connecting to a network.
+     */
+    public static boolean hasAvailableNetwork(Context context) {
+        ConnectivityManager cm = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo activeNetwork = cm.getActiveNetworkInfo();
+        return activeNetwork != null && activeNetwork.isConnectedOrConnecting();
+    }
+
+
+    /**
+     * Dismiss the virtual keyboard if it is visible.
+     *
+     * @param activity the activity which is having the keyboard.
+     */
+    public static void dismissKeyboard(Activity activity) {
+        InputMethodManager imm = (InputMethodManager) activity.getSystemService(Context.INPUT_METHOD_SERVICE);
+
+        if (activity.getWindow().getCurrentFocus() != null) {
+            imm.hideSoftInputFromWindow(activity.getWindow().getCurrentFocus().getWindowToken(), 0);
+        }
+    }
+
+    /**
+     * Exit the app and start it again.
+     *
+     * @param context       Application context.
+     * @param delayMilliSec Delay time with want to start application in milli seconds.
+     */
+    public static void scheduleForStart(Context context, int delayMilliSec) {
+        if (delayMilliSec < 100) {
+            delayMilliSec = 100;
+        }
+
+        Intent restartIntent = context.getPackageManager()
+                .getLaunchIntentForPackage(context.getPackageName());
+        PendingIntent intent = PendingIntent.getActivity(
+                context, 0,
+                restartIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+        AlarmManager manager = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
+        manager.set(AlarmManager.RTC, System.currentTimeMillis() + delayMilliSec, intent);
+    }
+
+
+    /**
+     * Stops all the running service for the current application
+     *
+     * @param context
+     */
+    public static void stopAllRunningServices(Context context) {
+
+        ActivityManager activityManager = (ActivityManager)
+                context.getSystemService(Context.ACTIVITY_SERVICE);
+
+        List<ActivityManager.RunningServiceInfo>
+                runningServiceInfoList = activityManager.getRunningServices(Integer.MAX_VALUE);
+
+        for (ActivityManager.RunningServiceInfo runningServiceInfo : runningServiceInfoList) {
+            String serviceName = runningServiceInfo.service.getClassName();
+            //TODO: should use context.getPackageName(),
+            // but since the package in manifest is not well formatted, it ok for now.
+            if (serviceName.contains("com.triaged.badge")) {
+                try {
+                    Class serviceClass = Class.forName(serviceName);
+                    context.stopService(new Intent(context, serviceClass));
+                } catch (ClassNotFoundException e) {
+                    App.gLogger.e(e);
+                }
+            }
+        }
+
+
+    }
+}
---
+++ app/src/main/java/com/triaged/utils/SharedPreferencesUtil.java
@@ -0,0 +1,83 @@
+package com.triaged.utils;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+
+import com.triaged.badge.app.App;
+
+
+/**
+ * Util class which provides easy access to shared preferences.
+ * <p/>
+ * Created by Sadegh Kazemy on 9/7/14.
+ */
+public class SharedPreferencesUtil {
+
+    private static final Context CONTEXT = App.context();
+    private static SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(CONTEXT);
+
+    public static boolean store(String key, Object value) {
+        if (value instanceof Boolean) {
+            return sharedPreferences.edit().putBoolean(key, (Boolean) value).commit();
+        } else if (value instanceof Integer) {
+            return sharedPreferences.edit().putInt(key, (Integer) value).commit();
+
+        } else if (value instanceof Float) {
+            return sharedPreferences.edit().putFloat(key, (Float) value).commit();
+        } else if (value instanceof String) {
+            return sharedPreferences.edit().putString(key, (String) value).commit();
+        } else if (value instanceof Long) {
+            return sharedPreferences.edit().putLong(key, (Long) value).commit();
+        }
+        return false;
+    }
+
+    public static boolean store(int keyResourceId, Object value) {
+        return store(CONTEXT.getString(keyResourceId), value);
+    }
+
+    public static boolean getBoolean(String key, boolean defaultValue) {
+        return sharedPreferences.getBoolean(key, defaultValue);
+    }
+
+    public static String getString(String key, String defaultValue) {
+        return sharedPreferences.getString(key, defaultValue);
+    }
+
+    public static float getFloat(String key, float defaultValue) {
+        return sharedPreferences.getFloat(key, defaultValue);
+    }
+
+    public static int getInteger(String key, int defaultValue) {
+        return sharedPreferences.getInt(key, defaultValue);
+    }
+
+    public static long getLong(String key, long defaultValue) {
+        return sharedPreferences.getLong(key, defaultValue);
+    }
+
+    public static boolean getBoolean(int keyResourceId, boolean defaultValue) {
+        return getBoolean(CONTEXT.getString(keyResourceId), defaultValue);
+    }
+
+    public static String getString(int keyResourceId, String defaultValue) {
+        return getString(CONTEXT.getString(keyResourceId), defaultValue);
+    }
+
+    public static float getFloat(int keyResourceId, float defaultValue) {
+        return getFloat(CONTEXT.getString(keyResourceId), defaultValue);
+    }
+
+    public static int getInteger(int keyResourceId, int defaultValue) {
+        return getInteger(CONTEXT.getString(keyResourceId), defaultValue);
+    }
+
+    public static long getLong(int keyResourceId, long defaultValue) {
+        return getLong(CONTEXT.getString(keyResourceId), defaultValue);
+    }
+
+    public static Boolean clearSharedPref() {
+        return sharedPreferences.edit().clear().commit();
+    }
+}
