26aa8b1eadb8e6381b8e9243bbb4ed46db4d5f3c
Sadegh Kazemy <sadegh.kazemy@gmail.com>
2014-09-08 17:57:58 +0430

    Added some indexes on each table.
    Also did some little refactoring.

--- app/src/main/java/com/triaged/badge/database/table/DepartmentsTable.java
+++ app/src/main/java/com/triaged/badge/database/table/DepartmentsTable.java
@@ -3,6 +3,7 @@ package com.triaged.badge.database.table;
 import android.database.sqlite.SQLiteDatabase;
 
 /**
+ *
  * Created by Sadegh Kazemy on 9/7/14.
  */
 public class DepartmentsTable extends AbstractTable {
@@ -12,24 +13,26 @@ public class DepartmentsTable extends AbstractTable {
     public static final String COLUMN_DEPARTMENT_NAME = "name";
     public static final String COLUMN_DEPARTMENT_NUM_CONTACTS = "num_contacts";
 
-    protected static final String CREATE_DEPARTMENTS_TABLE_SQL = String.format(
-            "create table %s (%s  integer primary key, %s text, %s integer );",
-            TABLE_NAME,
-            COLUMN_ID,
-            COLUMN_DEPARTMENT_NAME,
-            COLUMN_DEPARTMENT_NUM_CONTACTS
-    );
-
-    protected static final String DROP_DEPARTMENTS_TABLE_SQL = String.format("DROP TABLE IF EXISTS %s", TABLE_NAME);
-
     @Override
     public void onCreate(SQLiteDatabase db) {
-        db.execSQL(CREATE_DEPARTMENTS_TABLE_SQL);
+
+        StringBuilder createSql = new StringBuilder();
+        createSql.append("CREATE TABLE ").append(TABLE_NAME).append("(")
+                .append(COLUMN_ID).append(" INTEGER PRIMARY KEY AUTOINCREMENT, ")
+                .append(COLUMN_DEPARTMENT_NAME).append(" TEXT, ")
+                .append(COLUMN_DEPARTMENT_NUM_CONTACTS).append(" INTEGER")
+                .append(");");
+
+        db.execSQL(createSql.toString());
+
+        db.execSQL("CREATE INDEX department_id_index ON " + TABLE_NAME + "(" + COLUMN_ID + ");");
+        db.execSQL("CREATE INDEX department_num_contacts_index ON " + TABLE_NAME + "(" + COLUMN_DEPARTMENT_NUM_CONTACTS + ");");
+
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-        db.execSQL(DROP_DEPARTMENTS_TABLE_SQL);
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
     }
 
 }
