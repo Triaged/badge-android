c9a5458ab6558de97bb0bf3d9aaf39cce56bda24
Sadegh Kazemy <sadegh.kazemy@gmail.com>
2014-09-19 21:34:58 +0430

    Add OfficeLocation model.

--- app/src/main/java/com/triaged/badge/models/OfficeLocation.java
+++ app/src/main/java/com/triaged/badge/models/OfficeLocation.java
@@ -1,9 +1,89 @@
 package com.triaged.badge.models;
 
 /**
- * POJO for an office location within a company.
- * <p/>
- * Created by Will on 7/14/14.
+ * Created by Sadegh Kazemy on 9/19/14.
  */
 public class OfficeLocation {
+
+    int id;
+    String name;
+    String streetAddress;
+    String city;
+    String state;
+    String zipCode;
+    String country;
+    String latitude;
+    String longitude;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getStreetAddress() {
+        return streetAddress;
+    }
+
+    public void setStreetAddress(String streetAddress) {
+        this.streetAddress = streetAddress;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getZipCode() {
+        return zipCode;
+    }
+
+    public void setZipCode(String zipCode) {
+        this.zipCode = zipCode;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(String latitude) {
+        this.latitude = latitude;
+    }
+
+    public String getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(String longitude) {
+        this.longitude = longitude;
+    }
 }
