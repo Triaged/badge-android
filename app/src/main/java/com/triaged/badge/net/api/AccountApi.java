3c4c314c0567b4cdb0a63b2f9d9251011aa0c3fd
Sadegh Kazemy <sadegh.kazemy@gmail.com>
2014-09-22 12:32:31 +0330

    Added "update" and "uploadAvatar" to the account api client.

--- app/src/main/java/com/triaged/badge/net/api/AccountApi.java
+++ app/src/main/java/com/triaged/badge/net/api/AccountApi.java
@@ -2,10 +2,16 @@ package com.triaged.badge.net.api;
 
 import com.squareup.okhttp.Response;
 import com.triaged.badge.TypedJsonString;
+import com.triaged.badge.models.Account;
 
 import retrofit.Callback;
 import retrofit.http.Body;
+import retrofit.http.Multipart;
+import retrofit.http.PATCH;
+import retrofit.http.POST;
 import retrofit.http.PUT;
+import retrofit.http.Part;
+import retrofit.mime.TypedFile;
 
 /**
  * Created by Sadegh Kazemy on 9/19/14.
@@ -14,4 +20,11 @@ public interface AccountApi {
 
     @PUT("/v1/account/update_password")
     void changePassword(@Body TypedJsonString typedJsonString, Callback<Response> callback);
+
+    @PATCH("/v1/account")
+    void update(@Body TypedJsonString updateDate, Callback<Account> callback);
+
+    @Multipart
+    @POST("/v1/account/avatar")
+    void postAvatar(@Part("user[avatar]")TypedFile avatar, Callback<Account> callback);
 }
